d6bef3f4b9b6851e0045a22ca0d6e9f5adb76b16
Arslan Okenov <gtg100900@gmail.com>
2022-11-15 02:16:10 +0600

    added component Alert.tsx with logic and finished 58 h/w

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,13 @@ function App() {
   const [showAlert, setShowAlert] = useState(false);
 
    const closeModal = () => setShowModal(false);
+   const closeAlert = () => setShowAlert(false);
+
 
   return (
-    <>
+    <div className='m-auto w-50 mt-4'>
       <div className="modal-div">
-        <button className='btn btn-primary'  onClick={() => setShowModal(true)}>Show modal</button>
+        <button className='btn btn-primary mb-2'  onClick={() => setShowModal(true)}>Show modal</button>
         <Modal
           show={showModal}
           title="Some kinda modal title"
@@ -22,11 +24,13 @@ function App() {
       </div>
 
       <div className="alert-div">
-        <Alert/>
+        <button className='btn btn-primary mb-2'  onClick={() => setShowAlert(true)}>Show Alert</button>
+        <Alert type='warning' onDismiss={closeAlert} showAlert={showAlert}>This is a warning type alert</Alert>
+        <Alert type="success" onDismiss={closeAlert} showAlert={showAlert}>This is a success type alert</Alert>
+        <Alert type="danger" onDismiss={closeAlert} showAlert={showAlert}>This is a danger type alert</Alert>
+        <Alert type="primary" onDismiss={closeAlert} showAlert={showAlert}>This is a primary type alert</Alert>
       </div>
-    </>
-
-
+    </div>
   );
 }
 
--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {PropsWithChildren} from 'react';
+interface Props extends PropsWithChildren {
+  type:string;
+  showAlert:boolean;
+  onDismiss? :React.MouseEventHandler;
+}
 
-const Alert = () => {
+const Alert:React.FC<Props> = ({type,onDismiss,showAlert,  children}) => {
+  if (type === 'warning' || type === 'danger') {
+    return (
+      <>
+        <div className={"alert alert-" + type+ ' justify-content-between'} style={{display: showAlert ? 'flex': 'none'}} role='alert'>
+          {children}
+          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={onDismiss}></button>
+        </div>
+      </>
+    );
+  }
   return (
     <>
-      alert
+      <div className={"alert alert-" + type} style={{display: showAlert ? 'block': 'none'}} role="alert" onClick={onDismiss}>
+        {children}
+      </div>
     </>
   );
 };
